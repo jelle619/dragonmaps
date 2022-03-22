5be610e6476ca44c7b646a40e52398278b62392c
Jelle Z <public@jellezwie.rs>
2022-03-23 00:55:01 +0100

    Remove dummy marker/circle

--- script.js
+++ script.js
@@ -62,16 +62,5 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Set attribution
 L.control.attribution().setPrefix("").addAttribution('Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors').addTo(map);
 
-// Set circle
-var circle = L.circle([51.690486599927794, 5.296194152088851], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 556.5995
-}).addTo(map);
-
-// Set markers
-var marker = L.marker([51.690486599927794, 5.296194152088851]).addTo(map);
-
 // Add entries
 displayEntries();
